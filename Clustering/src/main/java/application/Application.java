696e93d85cc3444ce7bb06a3308bfc37099c6276
Dennis <krausdennis@gmx.de>
2019-04-21 20:41:26 +0200

    init

--- Clustering/src/main/java/application/Application.java
+++ Clustering/src/main/java/application/Application.java
@@ -2,8 +2,13 @@ package application;
 
 public class Application {
 
+	/***
+	 * 
+	 * Starting point of the application
+	 * 
+	 * @param args
+	 */
 	public static void main(String args[]) {
-	
-	}
+		}
 
 }
